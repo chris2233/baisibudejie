b5638315ed4332ad484f8bea420e01900c4ae1a8
chriszhangjie <13601115771@163.com>
2016-05-09 14:43:53 +0800

    xcode环境改变

---
+++ 百思不得姐/百思不得姐/Classes/Other/Controllers/ZJNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ZJNavigationController.h
+//  百思不得姐
+//
+//  Created by 张杰 on 16/5/9.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJNavigationController : UINavigationController
+
+@end
---
+++ 百思不得姐/百思不得姐/Classes/Other/Controllers/ZJNavigationController.m
@@ -0,0 +1,26 @@
+//
+//  ZJNavigationController.m
+//  百思不得姐
+//
+//  Created by 张杰 on 16/5/9.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import "ZJNavigationController.h"
+
+@interface ZJNavigationController ()
+
+@end
+
+@implementation ZJNavigationController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+-(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    [super pushViewController:viewController animated:YES];
+}
+@end
